ccfda311ad4be977979757e7407411e7a00078d0
arzzcom <arzz@arzz.com>
2018-03-21 03:56:03 +0900

    설정폼 수정

--- admin/configs.php
+++ admin/configs.php
@@ -1,15 +1,15 @@
 <?php
 /**
- * 이 파일은 iModule 포트폴리오모듈의 일부입니다. (https://www.imodule.kr)
+ * 이 파일은 iModule 위지윅에디터모듈 일부입니다. (https://www.imodule.kr)
  *
  * 위지윅에디터 설정을 위한 설정폼을 생성한다.
  * 
  * @file /modules/wysiwyg/admin/configs.php
  * @author Arzz (arzz@arzz.com)
- * @license MIT License
- * @version 3.0.0.160910
+ * @license GPLv3
+ * @version 3.0.0
+ * @modified 2018. 3. 18.
  */
-
 if (defined('__IM__') == false) exit;
 ?>
 <script>
@@ -17,6 +17,7 @@ var config = new Ext.form.Panel({
 	id:"ModuleConfigForm",
 	border:false,
 	bodyPadding:10,
+	width:600,
 	fieldDefaults:{labelAlign:"right",labelWidth:100,anchor:"100%",allowBlank:true},
 	items:[
 		new Ext.form.FieldSet({
@@ -25,10 +26,8 @@ var config = new Ext.form.Panel({
 				new Ext.form.TextArea({
 					fieldLabel:Wysiwyg.getText("admin/configs/form/iframe"),
 					name:"iframe",
-					height:200
-				}),
-				new Ext.form.DisplayField({
-					value:'<div class="x-form-help" style="padding-left:105px;">'+Wysiwyg.getText("admin/configs/form/iframe_help")+'</div>'
+					height:80,
+					afterBodyEl:'<div class="x-form-help">'+Wysiwyg.getText("admin/configs/form/iframe_help")+'</div>'
 				})
 			]
 		})
