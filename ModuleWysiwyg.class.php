008c7ee27076dd051148caa680b1939fadedb2ff
sangin <w.sangin2011@gmail.com>
2020-07-09 15:55:18 +0900

    웹접근성 처리

--- ModuleWysiwyg.class.php
+++ ModuleWysiwyg.class.php
@@ -5,7 +5,7 @@
  * 위지윅에디터와 관련된 모든 기능을 제어한다.
  * 위지윅에디터모듈에 포함된 Froala Wysiwyg Editor (https://froala.com/wysiwyg-editor) 는 iModule 내에서 자유롭게 사용할 수 있도록 라이센싱되어 있습니다.
  * iModule 외부에서 Froala Wysiwyg Editor 사용시 라이센스 위반이므로 주의하시기 바랍니다.
- * 
+ *
  * @file /modules/wysiwyg/ModuleWyiswyg.class.php
  * @author Arzz (arzz@arzz.com)
  * @license MIT License
@@ -18,16 +18,16 @@ class ModuleWysiwyg {
 	 */
 	private $IM;
 	private $Module;
-	
+
 	/**
 	 * 언어셋을 정의한다.
-	 * 
+	 *
 	 * @private object $lang 현재 사이트주소에서 설정된 언어셋
 	 * @private object $oLang package.json 에 의해 정의된 기본 언어셋
 	 */
 	private $lang = null;
 	private $oLang = null;
-	
+
 	/**
 	 * 위지윅에디터 설정변수
 	 */
@@ -47,12 +47,12 @@ class ModuleWysiwyg {
 	private $_files = array();
 	private $_disabled = false;
 	private $_codeMirror = array('xml');
-	
+
 	/**
 	 * HTMLPurifier
 	 */
 	private $_HTMLPurifier = null;
-	
+
 	/**
 	 * class 선언
 	 *
@@ -67,13 +67,13 @@ class ModuleWysiwyg {
 		 */
 		$this->IM = $IM;
 		$this->Module = $Module;
-		
+
 		/**
 		 * 첨부파일모듈 호출
 		 */
 		$this->_attachment = $this->IM->getModule('attachment');
 	}
-	
+
 	/**
 	 * 모듈 코어 클래스를 반환한다.
 	 * 현재 모듈의 각종 설정값이나 모듈의 package.json 설정값을 모듈 코어 클래스를 통해 확인할 수 있다.
@@ -83,7 +83,7 @@ class ModuleWysiwyg {
 	function getModule() {
 		return $this->Module;
 	}
-	
+
 	/**
 	 * [사이트관리자] 모듈 설정패널을 구성한다.
 	 *
@@ -95,15 +95,15 @@ class ModuleWysiwyg {
 		 */
 		$IM = $this->IM;
 		$Module = $this->getModule();
-		
+
 		ob_start();
 		INCLUDE $this->getModule()->getPath().'/admin/configs.php';
 		$panel = ob_get_contents();
 		ob_end_clean();
-		
+
 		return $panel;
 	}
-	
+
 	/**
 	 * 언어셋파일에 정의된 코드를 이용하여 사이트에 설정된 언어별로 텍스트를 반환한다.
 	 * 코드에 해당하는 문자열이 없을 경우 1차적으로 package.json 에 정의된 기본언어셋의 텍스트를 반환하고, 기본언어셋 텍스트도 없을 경우에는 코드를 그대로 반환한다.
@@ -124,10 +124,10 @@ class ModuleWysiwyg {
 				$this->oLang = null;
 			}
 		}
-		
+
 		$returnString = null;
 		$temp = explode('/',$code);
-		
+
 		$string = $this->lang;
 		for ($i=0, $loop=count($temp);$i<$loop;$i++) {
 			if (isset($string->{$temp[$i]}) == true) {
@@ -137,7 +137,7 @@ class ModuleWysiwyg {
 				break;
 			}
 		}
-		
+
 		if ($string != null) {
 			$returnString = $string;
 		} elseif ($this->oLang != null) {
@@ -152,10 +152,10 @@ class ModuleWysiwyg {
 					}
 				}
 			}
-			
+
 			if ($string != null) $returnString = $string;
 		}
-		
+
 		/**
 		 * 언어셋 텍스트가 없는경우 iModule 코어에서 불러온다.
 		 */
@@ -163,7 +163,7 @@ class ModuleWysiwyg {
 		elseif (in_array(reset($temp),array('text','button','action')) == true) return $this->IM->getText($code,$replacement);
 		else return $replacement == null ? $code : $replacement;
 	}
-	
+
 	/**
 	 * 상황에 맞게 에러코드를 반환한다.
 	 *
@@ -175,7 +175,7 @@ class ModuleWysiwyg {
 	function getErrorText($code,$value=null,$isRawData=false) {
 		$message = $this->getText('error/'.$code,$code);
 		if ($message == $code) return $this->IM->getErrorText($code,$value,null,$isRawData);
-		
+
 		$description = null;
 		switch ($code) {
 			case 'NOT_ALLOWED_SIGNUP' :
@@ -183,26 +183,26 @@ class ModuleWysiwyg {
 					$description = $value->title;
 				}
 				break;
-				
+
 			case 'DISABLED_LOGIN' :
 				if ($value != null && is_numeric($value) == true) {
 					$description = str_replace('{SECOND}',$value,$this->getText('text/remain_time_second'));
 				}
 				break;
-			
+
 			default :
 				if (is_object($value) == false && $value) $description = $value;
 		}
-		
+
 		$error = new stdClass();
 		$error->message = $message;
 		$error->description = $description;
 		$error->type = 'BACK';
-		
+
 		if ($isRawData === true) return $error;
 		else return $this->IM->getErrorText($error);
 	}
-	
+
 	/**
 	 * 위지윅에디터 설정값을 초기화한다.
 	 */
@@ -220,7 +220,7 @@ class ModuleWysiwyg {
 		$this->_files = array();
 		$this->__disabled = false;
 	}
-	
+
 	/**
 	 * 위지윅에디터 아이디를 설정한다.
 	 *
@@ -229,10 +229,10 @@ class ModuleWysiwyg {
 	 */
 	function setId($id) {
 		$this->_id = $id;
-		
+
 		return $this;
 	}
-	
+
 	/**
 	 * 위지윅에디터 TEXTAREA 의 NAME을 설정한다.
 	 *
@@ -242,10 +242,10 @@ class ModuleWysiwyg {
 	function setName($name) {
 		$this->_name = $name;
 		$this->_attachment->setWysiwyg($name);
-		
+
 		return $this;
 	}
-	
+
 	/**
 	 * 위지윅에디터를 호출한 모듈명을 설정한다.
 	 * 위지윅에디터에서 직접적으로 사용되지 않고, 위지윅에디터와 함께 사용되는 첨부파일모듈에서 사용된다.
@@ -256,10 +256,10 @@ class ModuleWysiwyg {
 	function setModule($module) {
 		$this->_module = $module;
 		$this->_attachment->setModule($module);
-		
+
 		return $this;
 	}
-	
+
 	/**
 	 * 위지윅에디터 입력폼의 placeholder 값을 설정한다.
 	 *
@@ -268,10 +268,10 @@ class ModuleWysiwyg {
 	 */
 	function setPlaceholder($text) {
 		$this->_placeholderText = $text;
-		
+
 		return $this;
 	}
-	
+
 	function setContent($content) {
 		if (is_object($content) == true) {
 			$this->_content = $this->decodeContent($content->text,false);
@@ -279,56 +279,56 @@ class ModuleWysiwyg {
 		} else {
 			$this->_content = $this->decodeContent($content,false);
 		}
-		
+
 		return $this;
 	}
-	
+
 	function setRequired($required) {
 		$this->_required = $required;
-		
+
 		return $this;
 	}
-	
+
 	function setTheme($theme) {
 		$this->_theme = $theme;
-		
+
 		return $this;
 	}
-	
+
 	function getAttachment() {
 		return $this->_attachment;
 	}
-	
+
 	function setHeight($height) {
 		$this->_height = $height;
-		
+
 		return $this;
 	}
-	
+
 	function setFileUpload($uploadFile) {
 		$this->_fileUpload = $uploadFile;
-		
+
 		return $this;
 	}
-	
+
 	function getImageUpload($uploadImage) {
 		$this->_imageUpload = $uploadImage;
-		
+
 		return $this;
 	}
-	
+
 	function setHideButtons($hideButtons=array()) {
 		$this->_hideButtons = $hideButtons;
-		
+
 		return $this;
 	}
-	
+
 	function setToolBarFixed($toolbarFixed) {
 		$this->_toolBarFixed = $toolbarFixed;
-		
+
 		return $this;
 	}
-	
+
 	/**
 	 * CodeMirror 의 모드를 추가한다.
 	 *
@@ -338,10 +338,10 @@ class ModuleWysiwyg {
 	function addCodeMirrorMode($mode) {
 		$this->_codeMirror[] = $mode;
 		$this->_codeMirror = array_unique($this->_codeMirror);
-		
+
 		return $this;
 	}
-	
+
 	/**
 	 * 위지윅에디터를 사용하기 위한 필수요소를 미리 불러온다.
 	 */
@@ -350,19 +350,19 @@ class ModuleWysiwyg {
 		$this->IM->addHeadResource('style',$this->getModule()->getDir().'/styles/wysiwyg.css.php?theme='.$this->_theme);
 		$this->loadCodeMirror();
 	}
-	
+
 	/**
 	 * CodeMirror 라이브러리를 불러온다.
 	 */
 	function loadCodeMirror() {
 		$this->IM->addHeadResource('script',$this->getModule()->getDir().'/scripts/codemirror/codemirror.js');
 		$this->IM->addHeadResource('style',$this->getModule()->getDir().'/styles/codemirror/codemirror.css');
-		
+
 		foreach ($this->_codeMirror as $mode) {
 			$this->IM->addHeadResource('script',$this->getModule()->getDir().'/scripts/codemirror/mode/'.$mode.'.js');
 		}
 	}
-	
+
 	/**
 	 * 위지윅 에디터를 가져온다.
 	 *
@@ -370,36 +370,36 @@ class ModuleWysiwyg {
 	 */
 	function get($is_inline=false) {
 		$this->preload();
-		
+
 		$this->_id = $this->_id == null ? uniqid('wysiwyg-') : $this->_id;
 		$this->_name = $this->_name == null ? 'content' : $this->_name;
-		
+
 		if ($this->_disabled == true) {
 			if ($is_inline == true) return '<div id="'.$this->_id.'">'.($this->_content !== null ? $this->_content : '').'</div>';
-			else return '<textarea id="'.$this->_id.'" name="'.$this->_name.'" data-wysiwyg="FALSE"'.($this->_required == true ? ' data-required="required"' : '').''.($this->_placeholderText != null ? ' placeholder="'.$this->_placeholderText.'"' : '').' style="height:'.$this->_height.'px;">'.($this->_content !== null ? $this->_content : '').'</textarea>';
+			else return '<textarea id="'.$this->_id.'" name="'.$this->_name.'" title="내용" data-wysiwyg="FALSE"'.($this->_required == true ? ' data-required="required"' : '').''.($this->_placeholderText != null ? ' placeholder="'.$this->_placeholderText.'"' : '').' style="height:'.$this->_height.'px;">'.($this->_content !== null ? $this->_content : '').'</textarea>';
 		}
-		
+
 		$wysiwyg = PHP_EOL.'<div data-role="module" data-module="wysiwyg">'.PHP_EOL;
-		
+
 		if ($is_inline == true) {
 			$wysiwyg.= '<div id="'.$this->_id.'" name="'.$this->_name.'" data-name="'.$this->_name.'" data-wysiwyg="TRUE" data-wysiwyg-module="'.$this->_module.'" data-wysiwyg-file-upload="'.($this->_fileUpload == true ? 'TRUE' : 'FALSE').'" data-wysiwyg-image-upload="'.($this->_imageUpload == true ? 'TRUE' : 'FALSE').'">'.($this->_content !== null ? $this->_content : ($this->_placeholderText != null ? $this->_placeholderText : '')).'</div>'.PHP_EOL;
 		} else {
-			$wysiwyg.= '<textarea id="'.$this->_id.'" name="'.$this->_name.'" data-name="'.$this->_name.'" data-wysiwyg="TRUE" data-wysiwyg-module="'.$this->_module.'" data-wysiwyg-file-upload="'.($this->_fileUpload == true ? 'TRUE' : 'FALSE').'" data-wysiwyg-image-upload="'.($this->_imageUpload == true ? 'TRUE' : 'FALSE').'" data-wysiwyg-minHeight="'.$this->_height.'"'.($this->_required == true ? ' data-wysiwyg-required="required"' : '').''.($this->_placeholderText != null ? ' placeholder="'.$this->_placeholderText.'"' : '').'>'.($this->_content !== null ? $this->_content : '').'</textarea>'.PHP_EOL;
+			$wysiwyg.= '<textarea id="'.$this->_id.'" name="'.$this->_name.'"title="내용" data-name="'.$this->_name.'" data-wysiwyg="TRUE" data-wysiwyg-module="'.$this->_module.'" data-wysiwyg-file-upload="'.($this->_fileUpload == true ? 'TRUE' : 'FALSE').'" data-wysiwyg-image-upload="'.($this->_imageUpload == true ? 'TRUE' : 'FALSE').'" data-wysiwyg-minHeight="'.$this->_height.'"'.($this->_required == true ? ' data-wysiwyg-required="required"' : '').''.($this->_placeholderText != null ? ' placeholder="'.$this->_placeholderText.'"' : '').'>'.($this->_content !== null ? $this->_content : '').'</textarea>'.PHP_EOL;
 		}
 		$wysiwyg.= '<script>$(document).ready(function() { $("#'.$this->_id.'").wysiwyg(); });</script>'.PHP_EOL;
 		$wysiwyg.= '</div>'.PHP_EOL;
-		
+
 		if (is_array($this->_files) == true) {
 			foreach ($this->_files as $file) {
 				$wysiwyg.= '<input type="hidden" name="'.$this->_name.'_files[]" value="'.Encoder($file).'">'.PHP_EOL;
 			}
 		}
-		
+
 		$this->reset();
-		
+
 		return $wysiwyg;
 	}
-	
+
 	/**
 	 * 위지윅 에디터를 비활성화한다.
 	 *
@@ -409,14 +409,14 @@ class ModuleWysiwyg {
 		$this->_disabled = true;
 		return $this;
 	}
-	
+
 	/**
 	 * 위지윅 에디터를 출력한다.
 	 */
 	function doLayout($is_inline=false) {
 		echo $this->get($is_inline);
 	}
-	
+
 	/**
 	 * XSS 공격방지 처리 클래스를 가져온다.
 	 *
@@ -424,7 +424,7 @@ class ModuleWysiwyg {
 	 */
 	function getHTMLPurifier() {
 		if ($this->_HTMLPurifier != null) return $this->_HTMLPurifier;
-		
+
 		REQUIRE_ONCE __IM_PATH__.'/classes/HTMLPurifier/HTMLPurifier.auto.php';
 
 		$config = HTMLPurifier_Config::createDefault();
@@ -442,13 +442,13 @@ class ModuleWysiwyg {
 		$config->set('HTML.SafeIframe',true);
 		$config->set('HTML.SafeObject',true);
 		$config->set('Output.FlashCompat',true);
-		
+
 		$iframe = explode("\n",str_replace(array('.'),array('\\.'),$this->getModule()->getConfig('iframe')));
 		$config->set('URI.SafeIframeRegexp', '#^(?:https?:)?//(?:'.implode('|',$iframe).')#');
 
 		$def = $config->getHTMLDefinition(true);
 		$def->addAttribute('img','usemap','CDATA');
-		
+
 		$map = $def->addElement('map','Block','Flow','Common',array('name'=>'CDATA'));
 		$map->excludes = array('map'=>true);
 
@@ -462,10 +462,10 @@ class ModuleWysiwyg {
 		));
 
 		$this->_HTMLPurifier = new HTMLPurifier($config);
-		
+
 		return $this->_HTMLPurifier;
 	}
-	
+
 	/**
 	 * 위지윅에디터에서 넘어온 내용을 가져온다.
 	 *
@@ -475,14 +475,14 @@ class ModuleWysiwyg {
 	function getContent($name,$is_json=true) {
 		$content = Request($name);
 		$files = Request($name.'_files');
-		
+
 		$data = new stdClass();
 		$data->text = $this->encodeContent($content,$files,true);
 		$data->files = $files;
-		
+
 		return $is_json == true ? json_encode($data,JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK) : $data;
 	}
-	
+
 	/**
 	 * 위지윅에디터의 내용을 정리한다.
 	 *
@@ -492,13 +492,13 @@ class ModuleWysiwyg {
 	 */
 	function encodeContent($content,&$attachments=array(),$autoRemove=false) {
 		$inserted = array();
-		
+
 		for ($i=0, $loop=count($attachments);$i<$loop;$i++) {
 			if (is_numeric($attachments[$i]) == false) {
 				$attachments[$i] = Decoder($attachments[$i]);
 			}
 		}
-		
+
 		if (preg_match_all('/<img([^>]*)data-idx="([0-9]+)"([^>]*)>/',$content,$match,PREG_SET_ORDER) == true) {
 			for ($i=0, $loop=count($match);$i<$loop;$i++) {
 				if (in_array($match[$i][2],$attachments) == true) {
@@ -520,7 +520,7 @@ class ModuleWysiwyg {
 				}
 			}
 		}
-		
+
 		if (preg_match_all('/<a([^>]*)data-idx="([0-9]+)"([^>]*)>/',$content,$match,PREG_SET_ORDER) == true) {
 			for ($i=0, $loop=count($match);$i<$loop;$i++) {
 				if (in_array($match[$i][2],$attachments) == true) {
@@ -542,7 +542,7 @@ class ModuleWysiwyg {
 				}
 			}
 		}
-		
+
 		if ($autoRemove == true && is_array($attachments) == true) {
 			foreach ($attachments as $attachment) {
 				if (in_array($attachment,$inserted) == true) {
@@ -551,13 +551,13 @@ class ModuleWysiwyg {
 					$this->IM->getModule('attachment')->fileDelete($attachment);
 				}
 			}
-			
+
 			$attachments = $inserted;
 		}
-		
+
 		return $content;
 	}
-	
+
 	/**
 	 * 위지윅에디터 내용 출력을 위해 내용을 정리한다.
 	 * 공격코드제거(AntiXSS) 및 첨부파일 정리, 스타일시트 적용
@@ -577,7 +577,7 @@ class ModuleWysiwyg {
 				$content = str_replace($match[$i][0],$image,$content);
 			}
 		}
-		
+
 		if (preg_match_all('/<a([^>]*)data-idx="([0-9]+)"([^>]*)>/',$content,$match,PREG_SET_ORDER) == true) {
 			for ($i=0, $loop=count($match);$i<$loop;$i++) {
 				$file = $this->IM->getModule('attachment')->getFileInfo($match[$i][2]);
@@ -589,12 +589,12 @@ class ModuleWysiwyg {
 				$content = str_replace($match[$i][0],$link,$content);
 			}
 		}
-		
+
 		if ($is_purify == true) {
 			$content = $this->getHTMLPurifier()->purify($content);
 			$content = PHP_EOL.'<div data-role="wysiwyg-content">'.$content.'</div>'.PHP_EOL;
 		}
-		
+
 		return $content;
 	}
 }
